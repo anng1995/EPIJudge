389150d4127cbaec1bf86650e1185fe116ee0b10
anng1995 <anthng95@gmail.com>
2020-03-30 11:24:42 -0700

    buy_and_sell_stock_twice O(N) time and space)

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -4,8 +4,27 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    max_total_profit, lowest_price = 0.0, float('inf')
+    first_stock_sell_profits = [0.0] * len(prices)
+
+    for idx, price in enumerate(prices):
+        if lowest_price > price:
+            lowest_price = price
+        else:
+            max_total_profit = max(max_total_profit, price - lowest_price)
+        first_stock_sell_profits[idx] = max_total_profit
+
+    current_max_price = float('-inf')
+
+    for i, price in reversed(list(enumerate(prices[1:], 1))):
+        if current_max_price < price:
+            current_max_price = price
+        else:
+            max_total_profit = max(
+                    max_total_profit,
+                    current_max_price - price + first_stock_sell_profits[i-1])
+
+    return max_total_profit
 
 
 if __name__ == '__main__':
