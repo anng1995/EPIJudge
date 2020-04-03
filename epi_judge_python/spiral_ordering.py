fa097ad745bd25a8a6f1f5c23f10bb2fa6a442b7
anng1995 <anthng95@gmail.com>
2020-04-03 12:02:01 -0700

    spiral_ordering O(n^2) run time)

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -5,7 +5,26 @@ from test_framework import generic_test
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    if len(square_matrix) == 1:
+        return square_matrix[0]
+
+    ret = []
+    left, right, up, down = 0, len(square_matrix)-1, len(square_matrix)-1, 0
+
+    while left < right and down < up:
+        ret.extend([square_matrix[down][i] for i in range(left, right)])
+        ret.extend([square_matrix[i][right] for i in range(down, up)])
+        ret.extend([square_matrix[up][i] for i in range(right, left, -1)])
+        ret.extend([square_matrix[i][left] for i in range(up, down, -1)])
+        left, right, up, down = left+1, right-1, up-1, down+1
+
+    length = len(square_matrix) % 2
+    half = len(square_matrix) // 2
+
+    if length == 1:
+        ret.extend([square_matrix[half][half]])
+
+    return ret
 
 
 if __name__ == '__main__':
